f407c52419142609ecda2192c3f255cf2128eb62
plooshi <95927318+plooshi@users.noreply.github.com>
2022-11-28 12:04:04 -0500

    vdfgn

--- src/ibootpatchfinder64_iOS14.cpp
+++ src/ibootpatchfinder64_iOS14.cpp
@@ -175,7 +175,7 @@ std::vector<patch> ibootpatchfinder64_iOS14::local_boot_patch(){
     loc_t debug_uartsrefstr = find_literal_ref(debug_uartsstr);
     debug("debug-uartsrefstr=%p",debug_uartsrefstr);
     
-    loc_t funccallrefstr = debug_uartsrefstr+20;
+    loc_t funccallrefstr = debug_uartsrefstr+28;
     debug("funccallrefstr=%p",funccallrefstr);
     
     loc_t test = find_call_ref(funccallrefstr);
