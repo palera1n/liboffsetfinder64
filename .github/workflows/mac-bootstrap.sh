71b68805a78d3d123239054d4cfe44dd56556ec9
plooshi <95927318+plooshi@users.noreply.github.com>
2022-11-29 12:55:10 -0500

    wait this ratio crypt

--- .github/workflows/mac-bootstrap.sh
+++ .github/workflows/mac-bootstrap.sh
@@ -17,13 +17,11 @@ echo "${PROCURSUS}/bin" | sudo tee /etc/paths1
 echo "${PROCURSUS}/libexec/gnubin" | sudo tee /etc/paths1
 cat /etc/paths | sudo tee -a /etc/paths1
 sudo mv /etc/paths{1,}
-wait
 mkdir -p ${DEP_ROOT}/macOS_x86_64_Release ${DEP_ROOT}/macOS_x86_64_Debug ${DEP_ROOT}/macOS_arm64_Release ${DEP_ROOT}/macOS_arm64_Debug
 gtar xf macOS_x86_64_Release_Latest.tar.zst -C ${DEP_ROOT}/macOS_x86_64_Release
 gtar xf macOS_x86_64_Debug_Latest.tar.zst -C ${DEP_ROOT}/macOS_x86_64_Debug
 gtar xf macOS_arm64_Release_Latest.tar.zst -C ${DEP_ROOT}/macOS_arm64_Release
 gtar xf macOS_arm64_Debug_Latest.tar.zst -C ${DEP_ROOT}/macOS_arm64_Debug
-wait
 # gtar xf ${BASE}/Builder_macOS.tar.zst &
 sudo ${PROCURSUS}/bin/apt-get update -qq
 sudo ${PROCURSUS}/bin/apt-get dist-upgrade -yqq
